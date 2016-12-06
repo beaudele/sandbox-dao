8318352786f176076a09b744c64a8587b3276ba4
Arnaud Beaudelet <beaudele@gmail.com>
2016-12-06 17:51:10 -0500

    Beautify code.

--- src/main/java/com/sandbox/springmvc/dao/UserDao.java
+++ src/main/java/com/sandbox/springmvc/dao/UserDao.java
@@ -1,25 +1,69 @@
 package com.sandbox.springmvc.dao;
 
-import java.util.List;
-
 import com.sandbox.springmvc.model.RegisteredUser;
 
-public interface UserDao {
-	
-	RegisteredUser findById(Long id);
-	
-	RegisteredUser findByName(String name);
-	
-	void saveUser(RegisteredUser user);
-	
-	void updateUser(RegisteredUser user);
-	
-	void deleteUserById(Long id);
+import java.util.List;
 
-	List<RegisteredUser> findAllUsers(); 
-	
-	void deleteAllUsers();
-	
-	public boolean isUserExist(RegisteredUser user);
+/**
+ * The Interface UserDao.
+ */
+public interface UserDao {
+
+  /**
+   * Find by id.
+   *
+   * @param id the id
+   * @return the registered user
+   */
+  RegisteredUser findById(Long id);
+
+  /**
+   * Find by name.
+   *
+   * @param name the name
+   * @return the registered user
+   */
+  RegisteredUser findByName(String name);
+
+  /**
+   * Save user.
+   *
+   * @param user the user
+   */
+  void saveUser(RegisteredUser user);
+
+  /**
+   * Update user.
+   *
+   * @param user the user
+   */
+  void updateUser(RegisteredUser user);
+
+  /**
+   * Delete user by id.
+   *
+   * @param id the id
+   */
+  void deleteUserById(Long id);
+
+  /**
+   * Find all users.
+   *
+   * @return the list
+   */
+  List<RegisteredUser> findAllUsers();
+
+  /**
+   * Delete all users.
+   */
+  void deleteAllUsers();
+
+  /**
+   * Checks if is user exist.
+   *
+   * @param user the user
+   * @return true, if is user exist
+   */
+  public boolean isUserExist(RegisteredUser user);
 
 }
